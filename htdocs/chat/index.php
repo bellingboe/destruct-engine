57225b108291b8fbcc6c980f7219f7b80323a79e
Brenden <brenden@brenden.me>
2014-02-26 06:38:12 -0600

    adding gogle analytics

--- htdocs/chat/index.php
+++ htdocs/chat/index.php
@@ -102,6 +102,16 @@ require_once '../../inc/Functions.php';
     
 	<script src="/combined.js"></script>
 	
+        <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                
+                ga('create', 'UA-27412705-2', 'destruct.co');
+                ga('send', 'pageview');
+        </script>
+	
 	<input type="hidden" id="curr_chat_pub_key">
     </body>
 </html>
--- htdocs/index.php
+++ htdocs/index.php
@@ -117,11 +117,16 @@ if ($url !== null) {
             </p>
 	</div>
         
+    <!--
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/jquery.js"><\/script>')</script>
         <script src="/js/core/plugins.js"></script>
 	<script src="/js/core/aes.js"></script>
         <script src="/js/core/main.js"></script>
+    -->
+    
+	<script src="/combined.js"></script>
+	
         <script>
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
