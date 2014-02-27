59f50ae64d2f83e9858d8766198699656af4a24f
Brenden <brenden@brenden.me>
2014-02-26 18:34:15 -0600

    twitter tracking

--- htdocs/chat/index.php
+++ htdocs/chat/index.php
@@ -26,121 +26,129 @@ require_once '../../inc/Functions.php';
     <meta name="twitter:app:id:googleplay" content="">
     <title>Chat</title>
 </head>
-    <body class="chat-main landing">
+     <body class="chat-main landing">
+      
+	  <!-- Google Tag Manager -->
+	  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MLWVL6"
+	  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	  '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	  })(window,document,'script','dataLayer','GTM-MLWVL6');</script>
+	  <!-- End Google Tag Manager -->
+      
+	  <header class="needs-logged-in">
+	     <div class="user-info needs-logged-in"></div>
+	     <div class="user-nav">
+		 <a href="/keys/logout.php?p=chat" class="box-clickable btn-red btn" id="do_logout">Logout</a>
+	     </div>
+	  </header>
      
-	<!-- Google Tag Manager -->
-	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MLWVL6"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-MLWVL6');</script>
-	<!-- End Google Tag Manager -->
-     
-	 <header class="needs-logged-in">
-	    <div class="user-info needs-logged-in"></div>
-	    <div class="user-nav">
-		<a href="/keys/logout.php?p=chat" class="box-clickable btn-red btn" id="do_logout">Logout</a>
-	    </div>
-	 </header>
-    
-	<div class="main-landing" id="login_signup">
-	    <h1 class="center-text header-callout landing-head">Login / Signup</h1>
-	    <form action="" method="POST" id="user_form" class="box-form">
-		<label>
-		    <span>Email</span> <input type="text" id="login_e">
-		</label>
-		<label>
-		    <span>Password</span> <input type="password" id="login_p">
-		</label>
-		<input type="submit" id="login_s" value="Submit">
-	    </form>
-	    <section class="box-form">
-		<h2>Some Tidbits</h2>
-		<ul>
-		    <li>
-			Enter an email address and a password. You will use them to login.
-		    </li>
-		    <li>
-			Each account gets an encrypted PGP keypair, locked with your password. 
-		    </li>
-		    <li>
-			The server never stores, or captures, your password except to hash it to login.
-		    </li>
-		    <li>
-			Every chat message is encrypted with your public, and your recipients. Not even I (<a class='btn btn-green btn-small' href='//twitter.com/NerdWhoCodes'>@NerdWhoCodes</a>) can read a single message.
-		    </li>
-		</ul>
-	    </section>
-	</div>
+	  <div class="main-landing" id="login_signup">
+	      <h1 class="center-text header-callout landing-head">Login / Signup</h1>
+	      <form action="" method="POST" id="user_form" class="box-form">
+		  <label>
+		      <span>Email</span> <input type="text" id="login_e">
+		  </label>
+		  <label>
+		      <span>Password</span> <input type="password" id="login_p">
+		  </label>
+		  <input type="submit" id="login_s" value="Submit">
+	      </form>
+	      <section class="box-form">
+		  <h2>Some Tidbits</h2>
+		  <ul>
+		      <li>
+			  Enter an email address and a password. You will use them to login.
+		      </li>
+		      <li>
+			  Each account gets an encrypted PGP keypair, locked with your password. 
+		      </li>
+		      <li>
+			  The server never stores, or captures, your password except to hash it to login.
+		      </li>
+		      <li>
+			  Every chat message is encrypted with your public, and your recipients. Not even I (<a class='btn btn-green btn-small' href='//twitter.com/NerdWhoCodes'>@NerdWhoCodes</a>) can read a single message.
+		      </li>
+		  </ul>
+	      </section>
+	  </div>
+	 
+	 <section class="needs-logged-in contacts-list">
+	     <span class="btn btn-green add-contact-wrapper">
+		 <div class="contact-search">Add Contact</div>
+		 <div class="contact-search-form form-group hide">
+		     <div class="close-btn contact-search-close">x</div>
+		     <input type="text" class="contact-search-text" placeholder="Search for FULL email address...">
+		     <div class="contact-search-results"></div>
+		 </div>
+	     </span>
+	     
+	     <div class="sep"></div>
+	     
+	     <div class="contact-header">Contacts</div>
+	     <div class="contact-list">None</div>
+	     
+	     <div class="contact-header">Contact Requests</div>
+	     <div class="contact-requests">None</div>
+	     
+	     <div class="contact-header">Sent Requests</div>
+	     <div class="contact-sent">None</div>
+	 </section>
 	
-	<section class="needs-logged-in contacts-list">
-	    <span class="btn btn-green add-contact-wrapper">
-		<div class="contact-search">Add Contact</div>
-		<div class="contact-search-form form-group hide">
-		    <div class="close-btn contact-search-close">x</div>
-		    <input type="text" class="contact-search-text" placeholder="Search for FULL email address...">
-		    <div class="contact-search-results"></div>
-		</div>
-	    </span>
-	    
-	    <div class="sep"></div>
-	    
-	    <div class="contact-header">Contacts</div>
-	    <div class="contact-list">None</div>
-	    
-	    <div class="contact-header">Contact Requests</div>
-	    <div class="contact-requests">None</div>
-	    
-	    <div class="contact-header">Sent Requests</div>
-	    <div class="contact-sent">None</div>
-	</section>
+	  <section class="needs-logged-in welcome-screen hide">
+	      <h1>Welcome</h1>
+	      <h2>Contacts</h2>
+	      <ul>
+		  <li>
+		      To add contacts, click on the "Add" button on the left, enter their <i>full</i> email address, then click on it when it appears.
+		  </li>
+		  <li>
+		      When they approve your request, they will appear under "Contacts". If they reject your request, you will no logner see their email in any list.
+		  </li>
+	      </ul>
+	  </section>
 	
-	<section class="needs-logged-in welcome-screen hide">
-	    <h1>Welcome</h1>
-	    <h2>Contacts</h2>
-	    <ul>
-		<li>
-		    To add contacts, click on the "Add" button on the left, enter their <i>full</i> email address, then click on it when it appears.
-		</li>
-		<li>
-		    When they approve your request, they will appear under "Contacts". If they reject your request, you will no logner see their email in any list.
-		</li>
-	    </ul>
-	</section>
-	
-	<section class="conversation-output hide">
-	    <h1 id="conversation-header" class="needs-active-chat"></h1>
-	    <div class="conversation-output-stream needs-active-chat"></div>
-	    <textarea class="conversation-text-input needs-active-chat"></textarea>
-	</section>
-	
-	<footer>
-	    <a class='btn' href='/'>A Destruct.co Experiment</a> made by <a class='btn btn-green' href='//twitter.com/NerdWhoCodes'>@NerdWhoCodes</a>
-	</footer>
-    <!--
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-        <script src="/js/plugins.js"></script>
-	<script src="/js/vendor/openpgp/openpgp.min.js"></script>
-	<script src="/js/aes.js"></script>
-        <script src="/js/chat-main.js"></script>
-    -->
-    
-        <script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                
-                ga('create', 'UA-27412705-2', 'destruct.co');
-		ga('require', 'linkid', 'linkid.js');
-                ga('send', 'pageview');
-        </script>
-	
-	<script src="/combined.js"></script>
-	
-	<input type="hidden" id="curr_chat_pub_key">
+	  <section class="conversation-output hide">
+	      <h1 id="conversation-header" class="needs-active-chat"></h1>
+	      <div class="conversation-output-stream needs-active-chat"></div>
+	      <textarea class="conversation-text-input needs-active-chat"></textarea>
+	  </section>
+	  
+	  <footer>
+	      <a class='btn' href='/'>A Destruct.co Experiment</a> made by <a class='btn btn-green' href='//twitter.com/NerdWhoCodes'>@NerdWhoCodes</a>
+	  </footer>
+	  <!--
+	  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	  <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+	  <script src="/js/plugins.js"></script>
+	  <script src="/js/vendor/openpgp/openpgp.min.js"></script>
+	  <script src="/js/aes.js"></script>
+	  <script src="/js/chat-main.js"></script>
+	  -->
+	  
+	  <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  
+		  ga('create', 'UA-27412705-2', 'destruct.co');
+		  ga('require', 'linkid', 'linkid.js');
+		  ga('send', 'pageview');
+	  </script>
+	  
+	  <script src="/combined.js"></script>
+	  
+	  <input type="hidden" id="curr_chat_pub_key">
+	  
+	  <script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
+	  <script type="text/javascript">
+	       twttr.conversion.trackPid('l4bnc');
+	  </script>
+	  <noscript>
+	       <img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=l4bnc&p_id=Twitter" />
+	  </noscript>
     </body>
 </html>
--- htdocs/index.php
+++ htdocs/index.php
@@ -130,26 +130,34 @@ if ($url !== null) {
             </p>
 	</div>
         
-    <!--
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="/js/jquery.js"><\/script>')</script>
-        <script src="/js/core/plugins.js"></script>
+	<!--
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="/js/jquery.js"><\/script>')</script>
+	<script src="/js/core/plugins.js"></script>
 	<script src="/js/core/aes.js"></script>
-        <script src="/js/core/main.js"></script>
-    -->
-
-        <script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                
-                ga('create', 'UA-27412705-2', 'destruct.co');
+	<script src="/js/core/main.js"></script>
+	-->
+	
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		ga('create', 'UA-27412705-2', 'destruct.co');
 		ga('require', 'linkid', 'linkid.js');
-                ga('send', 'pageview');
-        </script>
+		ga('send', 'pageview');
+	</script>
 	
 	<script src="/combined.js"></script>
+	
+	<script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	     twttr.conversion.trackPid('l4bnc');
+	</script>
+	<noscript>
+	     <img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=l4bnc&p_id=Twitter" />
+	</noscript>
 	
     </body>
 </html>
