450d1ac1cc41e1475a3873dbcf846b83c39beb3a
Brenden <brenden@brenden.me>
2014-03-03 13:26:47 -0600

    plain text header

--- htdocs/chat/dl.php
+++ htdocs/chat/dl.php
@@ -1,6 +1,8 @@
 <?php
 require_once '../../inc/Functions.php';
 
+header("Content-Type: text/plain");
+
 $msg_id = @$_GET['m'];
 $key = @$_GET['k'];
 
@@ -11,7 +13,12 @@ $msg_data = json_decode(base64_decode($msg->enc_text));
 echo "m->enc_text:\n\n";
 var_dump($msg->enc_text);
 
+echo "\n\n ----------------------------------- \n\n";
+
 echo "msg_data:\n\n";
 var_dump($msg_data);
 
 $dec = GibberishAES::dec($msg_data->t, $key);
+
+echo "dec:\n\n";
+var_dump($dec);
