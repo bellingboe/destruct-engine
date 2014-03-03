95b526769b51cda9d1f57c9fe452d08b9316d8ff
Brenden <brenden@brenden.me>
2014-03-03 14:09:10 -0600

    removing tracking data from chat app

--- htdocs/chat/index.php
+++ htdocs/chat/index.php
@@ -31,16 +31,6 @@ require_once '../../inc/Functions.php';
 </head>
      <body class="chat-main landing">
       
-	  <!-- Google Tag Manager -->
-	  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MLWVL6"
-	  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	  '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	  })(window,document,'script','dataLayer','GTM-MLWVL6');</script>
-	  <!-- End Google Tag Manager -->
-      
 	  <header class="needs-logged-in">
 	     <div class="user-info needs-logged-in"></div>
 	     <div class="user-nav">
@@ -128,38 +118,8 @@ require_once '../../inc/Functions.php';
 	<footer>
 	    <a class='btn' href='/'>A Destruct.co Experiment</a> made by <a class='btn btn-green' href='//twitter.com/NerdWhoCodes'>@NerdWhoCodes</a>
 	</footer>
-	<!--
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-	<script src="/js/plugins.js"></script>
-	<script src="/js/vendor/openpgp/openpgp.min.js"></script>
-	<script src="/js/aes.js"></script>
-	<script src="/js/chat-main.js"></script>
-	-->
-	
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		
-		ga('create', 'UA-27412705-2', 'destruct.co');
-		ga('require', 'linkid', 'linkid.js');
-		ga('send', 'pageview');
-	</script>
 	
 	<script src="/combined.js"></script>
-	
-	<!--
-	<script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
-	<script type="text/javascript">
-	     twttr.conversion.trackPid('l4bnc');
-	</script>
-	-->
-	
-	<noscript>
-	     <img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=l4bnc&p_id=Twitter" />
-	</noscript>
 	
 	<div class="sheet-msg-box"></div>
 	<div class="sheet-overlay"></div>
