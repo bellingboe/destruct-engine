e2d26593018a41513586793479c3772176d00a11
Brenden <brenden@brenden.me>
2014-03-03 13:43:53 -0600

    removed dumping in DL file, and updated CSS for dl

--- htdocs/chat/dl.php
+++ htdocs/chat/dl.php
@@ -7,18 +7,8 @@ $msg_id = @$_GET['m'];
 $key = @$_GET['k'];
 
 $msg = Message::find((int)$msg_id);
-
 $msg_data = json_decode(base64_decode($msg->enc_text));
-
-echo "m->enc_text:\n\n";
-var_dump($msg->enc_text);
-
-echo "\n\n ----------------------------------- \n\n";
-
-echo "msg_data:\n\n";
-var_dump($msg_data);
-
 $dec = GibberishAES::dec($msg_data->t, $key);
 
-echo "dec:\n\n";
-var_dump($dec);
+echo $dec;
+exit;
