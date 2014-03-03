7012e23dab3f15d84d355ba167ff0332f38b1573
Brenden <brenden@brenden.me>
2014-03-03 13:24:24 -0600

    php var dumping

--- htdocs/chat/dl.php
+++ htdocs/chat/dl.php
@@ -8,4 +8,10 @@ $msg = Message::find((int)$msg_id);
 
 $msg_data = json_decode(base64_decode($msg->enc_text));
 
-$dec = GibberishAES::dec($msg_data['t'], $key);
+echo "m->enc_text:\n\n";
+var_dump($msg->enc_text);
+
+echo "msg_data:\n\n";
+var_dump($msg_data);
+
+$dec = GibberishAES::dec($msg_data->t, $key);
