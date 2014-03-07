962d59470849bef079e79c4115a0cb35c6e3b6d6
Brenden <brenden@brenden.me>
2014-03-07 06:06:37 -0600

    new table scema

--- schema.sql
+++ schema.sql
@@ -39,16 +39,17 @@ CREATE TABLE `contacts` (
 -- The actual encrypted chat messages
 --
 
-DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
-  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
-  `contact_id` int(20) unsigned NOT NULL,
-  `user_id` int(20) unsigned NOT NULL,
-  `enc_text` text COLLATE utf8_unicode_ci NOT NULL,
-  `read_ts` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `sent_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+ `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+ `is_file` tinyint(1) NOT NULL DEFAULT '0',
+ `contact_id` int(20) unsigned NOT NULL,
+ `user_id` int(20) unsigned NOT NULL,
+ `enc_text` text COLLATE utf8_unicode_ci NOT NULL,
+ `enc_file` longblob NOT NULL,
+ `read_ts` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+ `sent_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
 
 -- --------------------------------------------------------
 
