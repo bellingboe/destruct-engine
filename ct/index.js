31c3ec7205dafea9f0b43ce42edd0aa9c4b2a4ee
Brenden Ellingboe <brenden@brenden.me>
2014-09-22 23:52:04 -0500

    removing old enables

--- ct/index.js
+++ ct/index.js
@@ -7,15 +7,9 @@ var app = require('express')()
       ca: fs.readFileSync('/etc/ssl/www_destruct_co.ca-bundle') 
     }, app)
   , io = require('socket.io')(https);
-  
-io.enable('browser client minification');  // send minified client
-io.enable('browser client etag');          // apply etag caching logic based on version number
-io.enable('browser client gzip');          // gzip the file
-io.set('log level', 1);                    // reduce logging
 
-// enable all transports (optional if you want flashsocket support, please note that some hosting
-// providers do not allow you to create servers that listen on a port different than 80 or their
-// default port)
+io.set('log level', 1);
+
 io.set('transports', [
     'websocket'
   , 'flashsocket'
