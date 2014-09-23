9371373f6d6c8286fb141983363ead04fec9e6b9
Brenden Ellingboe <brenden@brenden.me>
2014-09-22 23:51:12 -0500

    adding all transports

--- ct/index.js
+++ ct/index.js
@@ -7,6 +7,22 @@ var app = require('express')()
       ca: fs.readFileSync('/etc/ssl/www_destruct_co.ca-bundle') 
     }, app)
   , io = require('socket.io')(https);
+  
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set('log level', 1);                    // reduce logging
+
+// enable all transports (optional if you want flashsocket support, please note that some hosting
+// providers do not allow you to create servers that listen on a port different than 80 or their
+// default port)
+io.set('transports', [
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
