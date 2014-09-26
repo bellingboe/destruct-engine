80b5694413f2b8e9b5b9930721b3e87ccd0f8357
Brenden Ellingboe <bellingboe@digitalriver.com>
2014-09-26 09:54:48 -0500

    updating contacts

--- ct/index.js
+++ ct/index.js
@@ -40,34 +40,39 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 
-    socket.on('id-with-key', function(name, key){
-        users[socket.id] = name;
-        pubs[name] = key;
-        user_socks[name] = socket.id;
-        //io.emit('socket-from-key', id, socket.id);
-    });
+  socket.on('id-with-key', function(name, key){
+      users[socket.id] = name;
+      pubs[name] = key;
+      user_socks[name] = socket.id;
+      //io.emit('socket-from-key', id, socket.id);
+  });
 	
 	socket.on('verify-name', function(name){
 	  if ("undefined" !== typeof user_socks[name]) {
-		var user_pub = pubs[name];
-		socket.emit("verify-true", {name: name, key: user_pub});
+		  var user_pub = pubs[name];
+		  socket.emit("verify-true", {name: name, key: user_pub});
 	  } else {
-		socket.emit("verify-false", {name: name});
+		  socket.emit("verify-false", {name: name});
 	  }
 	});
 
-    socket.on("disconnect", function(){
-        try {
-        //var id = users[socket.id];
-        //io.emit('idDisconn', id, socket.id);
-          var name = users[socket.id];
-          delete pubs[name];
-          delete users[socket.id];
-		  delete user_socks[name];
-        } catch (e){}
-        });
+  socket.on("send-user-verify", function (user, contact) {
+    var c = getUserByName(contact);
+    io.sockets.socket(user_socks[c.sock]).emit("added-by-user", {"name": user});
+  });
 
-    });
+  socket.on("disconnect", function(){
+      try {
+      //var id = users[socket.id];
+      //io.emit('idDisconn', id, socket.id);
+        var name = users[socket.id];
+        delete pubs[name];
+        delete users[socket.id];
+       delete user_socks[name];
+      } catch (e){}
+  });
+
+});
 
 https.listen(3000, function(){
 console.log('listening on *:3000');
