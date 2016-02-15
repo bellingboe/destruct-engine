1d11148866b70a46ecfd7eee97f8d740b49d11d8
Brenden <brenden@brenden.me>
2016-02-15 16:51:20 -0500

    new schema

--- schema.sql
+++ schema.sql
@@ -13,10 +13,9 @@ CREATE TABLE `crypt_data` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `b` text COLLATE utf8_unicode_ci NOT NULL,
   `n` char(16) COLLATE utf8_unicode_ci NOT NULL,
-  `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-  PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB;
+  `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -30,9 +29,8 @@ CREATE TABLE `contacts` (
   `user_id` int(20) unsigned NOT NULL,
   `approved_ts` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `requested_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`contact_id`)
-)
-ENGINE=InnoDB;
+ PRIMARY KEY (contact_id)
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -49,9 +47,8 @@ CREATE TABLE `messages` (
  `enc_file` longblob NOT NULL,
  `read_ts` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
  `sent_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB;
+ PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -64,9 +61,8 @@ CREATE TABLE `users` (
   `email_address` varchar(320) COLLATE utf8_unicode_ci NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `acct_pw` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB;
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -79,10 +75,9 @@ CREATE TABLE IF NOT EXISTS `pubkeys` (
   `user_id` int(20) unsigned NOT NULL,
   `key_label` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `key_data` longtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (id),
   KEY `user_id` (`user_id`)
-)
-ENGINE=InnoDB;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -96,9 +91,8 @@ CREATE TABLE `pairs` (
   `key_label` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `key_data` longtext COLLATE utf8_unicode_ci NOT NULL,
   `pub_string` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (id),
   KEY `user_id` (`user_id`)
-)
-ENGINE=InnoDB;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
